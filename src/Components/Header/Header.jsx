1321e577282d8de7370779ba3b11e55537f32cdd
Mohammad Amrollahi <mohammadamrollahi3@gmail.com>
2022-08-29 16:05:33 +0430

    headbar layout develeped and db and assets added to project

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-import './style.scss'
+import React from "react";
+import "./style.scss";
+import starBoxLogo from "../../Additional/Vectors/starboxLogo.png";
+import seatchIcon from "../../Additional/Vectors/search-icon.svg";
+
 function Header() {
   return (
-    <div className='header-container'></div>
-  )
+    <div className="header-container">
+      <div className="header-leftInner-container">
+        <img className="seatch-icon" src={seatchIcon} alt="logo" />
+      </div>
+      <div className="header-rightInner-container">
+        <a href="#"className="menu-item ">همکاری</a>
+        <a href="#"className="menu-item">فروشگاه‌ها</a>
+        <a href="#"className="menu-item menu-selectedItem">منو</a>
+        <img className="logo" src={starBoxLogo} alt="logo" />
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
